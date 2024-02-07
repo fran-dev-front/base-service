00a322a8e7b714c76ed136b894f1b5b61301ab78
mitnik <mitnik@mitnik.mitnik_domain>
2024-02-06 21:13:13 -0600

    change on axios

--- src/baseService/baseService.js
+++ src/baseService/baseService.js
@@ -15,13 +15,28 @@ axios.interceptors.request.use(function (config) {
 axios.interceptors.response.use(function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
-    console.log(response)
+
     return response;
 }, function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    console.log('!!error', error)
-    return Promise.reject(error);
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response);
+        return error.response
+        // console.log(error.response.status);
+        // console.log(error.response.headers);
+    } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the 
+        // browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+    } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+        return error
+    }
+    console.log(error.config);
 });
 
 
